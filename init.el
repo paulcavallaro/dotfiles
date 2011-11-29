8f69e71a0226c5b84aa14fe3693de3993d3941c0
Paul Cavallaro <paulcavallaro@gmail.com>
2011-11-29 15:46:26 -0500

    Just kidding

--- init.el
+++ init.el
@@ -87,4 +87,5 @@
 ;; Find-File-In-Project Settings
 (require 'find-file-in-project)
 (setq ffip-limit 2048)
+(setq ffip-project-file ".emacs-project")
 (setq ffip-patterns (append '("*.hamlpy" "*.sass" "*.coffee") ffip-patterns))
