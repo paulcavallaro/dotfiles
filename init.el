cfb410943c24956474261473da47bcd078b8010d
Paul Cavallaro <pcavallaro@hioscar.com>
2017-04-14 12:25:21 -0400

    update init.el

--- init.el
+++ init.el
@@ -1,7 +1,102 @@
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
+
+;; Add unmanaged load path
+(add-to-list 'load-path "~/.emacs.d/unmanaged")
+(add-to-list 'load-path "~/.emacs.d/unmanaged/thrift.el")
+(add-to-list 'load-path "~/.emacs.d/unmanaged/highlight-80+.el")
+
+;; Change default font to Space Mono
+(set-frame-font "Space Mono-12")
+;; Old default
+;; (set-frame-font "Menlo-12")
+(make-face 'fancy-comment-face)
+(set-face-foreground 'fancy-comment-face "#586e75")
+(set-face-font 'fancy-comment-face "Space Mono-12:italic")
+
+;; Tramp handle 2fac auth
+(setq
+ tramp-password-prompt-regexp
+ (concat
+  "^.*"
+  (regexp-opt
+   '("passcode" "Passcode"
+     "password" "Password") t)
+  ".*:\0? *"))
+
+;; Tramp use sshx
+(setq tramp-default-method "sshx")
+
+;; Tramp over mosh for OMGWTFBBQ goodness
+;; -- Doesn't work, cause mosh is a remote terminal emulator
+;; -- more than it's a ssh replacement. Maybe some day.
+;; (add-to-list
+;;  'tramp-methods
+;;  '("mosh"
+;;    (tramp-remote-shell "/bin/sh")
+;;    (tramp-remote-shell-args ("-c"))
+;;    (tramp-login-program "/usr/local/bin/mosh")
+;;    (tramp-login-args (("%u@%h")))
+;;    (tramp-async-args (()))
+;;    (tramp-gw-args ())
+;;    (tramp-default-port 22)))
+
+;; Take that emacs version control integration!
+(setq vc-handled-backends nil)
+
+;; Turn off bell
+(defun ring-bell-function ())
+
+;; custom-set-variables for customize stuff
+;; ack.el fixes for git grep --no-color
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ack-command "git grep ")
+ '(ack-vc-grep-commands
+   (quote
+    ((".git" . "git --no-pager grep --no-color -n -i")
+     (".hg" . "hg grep -n -i")
+     (".bzr" . "bzr grep --color=always -n -i"))))
+ '(background-color "#7f7f7f")
+ '(background-mode dark)
+ '(cursor-color "#5c5cff")
+ '(custom-safe-themes
+   (quote
+    ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(foreground-color "#5c5cff")
+ '(package-selected-packages
+   (quote
+    (buffer-move multiple-cursors yaml-mode tuareg solarized-theme smex racer pylint pretty-lambdada php-mode paredit nlinum markdown-mode magit lua-mode jinja2-mode ido-ubiquitous go-mode ghci-completion ghc geiser flymake-go flymake-cursor flymake flycheck-rust find-file-in-project fic-ext-mode d-mode cm-mode clang-format browse-kill-ring ack ace-jump-mode))))
+
+;; super awesome project git grep from emacs
+(require 'ack)
+(global-set-key (kbd "M-F") 'ack)
+
+;; Revert all buffers visiting a file function
+(defun revert-all-buffers ()
+  "Refreshes all open buffers from their respective files."
+  (interactive)
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (when (and (buffer-file-name) (file-exists-p (buffer-file-name)) (not (buffer-modified-p)))
+        (revert-buffer t t t) )))
+  (message "Refreshed open files."))
+
+;; Give me my definition please and thank you
+(global-set-key (kbd "M-?") (lambda () (interactive) (find-tag (find-tag-default))))
+
+;; Make things kind of more intuitive
+(global-set-key (kbd "C-x |") (lambda () (interactive) (split-window-right)))
+(global-set-key (kbd "C-x _") (lambda () (interactive) (split-window-below)))
 
 ;; Hide menu bar
 (menu-bar-mode 0)
@@ -12,11 +107,26 @@
 ;; OS level copy / paste
 (setq x-select-enable-clipboard t)
 
+;; Revert buffer with no regard for human life
+(global-set-key (kbd "s-u") (lambda () (interactive) (revert-buffer t t)))
+
+;; Kill whole line
+(global-set-key (kbd "C-S-k") 'kill-whole-line)
+
+;; I hate you upcase region
+(global-set-key (kbd "C-x C-u") 'undo)
+
 ;; Scroll up with cursor
 (global-set-key (kbd "M-P") (lambda (arg) (interactive "p") (previous-line arg) (scroll-down-line arg)))
 
+;; Scroll up w/o cursor
+(global-set-key (kbd "M-p") 'scroll-down-line)
+
 ;; Scroll down with cursor
 (global-set-key (kbd "M-N") (lambda (arg) (interactive "p") (next-line arg) (scroll-up-line arg)))
+
+;; Scroll down w/o cursor
+(global-set-key (kbd "M-n") 'scroll-up-line)
 
 ;; Goto line
 (global-set-key (kbd "M-g") 'goto-line)
@@ -24,8 +134,17 @@
 ;; Go back a buffer
 (global-set-key (kbd "C-x p") (lambda () (interactive) (other-window -1)))
 
-(setq indent-tabs-mode 'nil)
-(setq whitespace-style '(face tabs spaces trailing lines space-before-tab::space newline indentation::space empty space-after-tab::space space-before-tab::space space-mark tab-mark newline-mark))
+;; Clean up tramp connections
+(global-set-key (kbd "C-x C-t") 'tramp-cleanup-all-connections)
+
+;; Paredit overrides C-j binding, add new binding
+(global-set-key (kbd "C-S-j") 'eval-print-last-sexp)
+
+;; Remap M-% to isearch-query-replace
+(global-set-key (kbd "M-%") 'isearch-query-replace-regexp)
+
+;; No tabs for you!
+(setq-default indent-tabs-mode nil)
 
 ;; Clean whitespace
 (global-set-key (kbd "C-x M-w") (lambda () (interactive)
@@ -33,11 +152,38 @@
 
 ;; Scala 2 Mode
 ;; https://github.com/hvesalai/scala-mode2
-(add-to-list 'load-path "~/.emacs.d/scala-mode2/")
-(require 'scala-mode)
+;; (add-to-list 'load-path "~/.emacs.d/scala-mode2/")
+;; (require 'scala-mode)
 
-;; C indentation level to 8
-(setq-default c-basic-offset 8)
+;; C indentation level to 4
+(setq-default c-basic-offset 4)
+
+(require 'cc-mode)
+
+(add-to-list 'c-default-style '(php-mode . "fb"))
+
+(c-add-style "fb" '("k&r"
+                    (c-basic-offset . 2)
+                    (c-hanging-braces-alist
+                     (defun-open after)
+                     (substatement-open after))
+                    (c-offsets-alist
+                     (arglist-close . 0)
+                     (arglist-intro . +))))
+
+(defun basic-c-mode-hook ()
+  "Basic C/C++ mode hook"
+  (c-set-style "fb")
+  (setq indent-tabs-mode nil)
+)
+
+(add-hook 'c-mode-hook (function (lambda () (basic-c-mode-hook))))
+(add-hook 'c++-mode-hook (function (lambda () (basic-c-mode-hook))))
+
+(add-hook 'php-mode-hook
+          (function (lambda ()
+                      (c-set-style "fb")
+                      (setq indent-tabs-mode nil))))
 
 (defun save-to-clipboard (start end)
   "Save region to clipboard through pbcopy"
@@ -48,12 +194,14 @@
 ;; Copy to Mac OS X Clipboard
 (global-set-key (kbd "M-W") 'save-to-clipboard)
 
-(setq linum-format "%d ")
-(global-linum-mode 1)
+(require 'nlinum)
+(setq nlinum-format "%d ")
+(global-nlinum-mode 1)
 (column-number-mode 1)
 (setq python-indent 4)
+(setq rust-indent-unit 4)
 
-(setq js-indent-level 4)
+(setq js-indent-level 2)
 
 ;; Disable auto-save and backups
 (setq backup-inhibited t)
@@ -65,7 +213,7 @@
             (set (make-local-variable 'sgml-basic-offset) 2)))
 
 ;; Adding geiser mode
-(load-file "~/lisp/geiser-0.2.1/elisp/geiser.el")
+;; (load-file "~/lisp/geiser-0.2.1/elisp/geiser.el")
 ;; Adding quack mode after geiser
 ;; (load-file "~/lisp/quack-0.45/quack.el")
 ;; (require 'quack)
@@ -74,6 +222,13 @@
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
+(setq ido-create-new-buffer 'prompt)
+
+;; smex (ido-mode for M-x)
+(autoload 'smex "smex"
+  "Smex is a M-x enhancement for Emacs, it provides a convenient interface to
+your recently and most frequently used commands.")
+(global-set-key (kbd "M-x") 'smex)
 
 ;; Pretty Lambda for Python
 (setq pretty-lambda-auto-modes '(lisp-mode emacs-lisp-mode lisp-interaction-mode scheme-mode python-mode))
@@ -97,6 +252,13 @@
 (setq ffip-patterns (append '("*.hamlpy" "*.sass" "*.coffee" "*.hs" "*.lhs" "*.hsc" "*.ocaml" "*.rkt" "*.scm" "*.proto" "*.java" "*.c" "*.h") ffip-patterns))
 (global-set-key (kbd "C-c C-f") 'ffip)
 
+;; Paredit Mode Keybindings
+(require 'paredit)
+(define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp)
+(define-key paredit-mode-map (kbd "M-(") 'paredit-forward-barf-sexp)
+(define-key paredit-mode-map (kbd "M-{") 'paredit-backward-slurp-sexp)
+(define-key paredit-mode-map (kbd "M-}") 'paredit-backward-barf-sexp)
+
 (autoload 'paredit-mode "paredit"
   "Minor mode for pseudo-structurally editing Lisp code." t)
 (add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
@@ -105,16 +267,10 @@
 (add-hook 'scheme-mode-hook           (lambda () (paredit-mode +1)))
 (add-hook 'clojure-mode-hook          (lambda () (paredit-mode +1)))
 
-;; Paredit Mode Keybindings
-(require 'paredit)
-(define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp)
-(define-key paredit-mode-map (kbd "M-(") 'paredit-forward-barf-sexp)
-(define-key paredit-mode-map (kbd "M-{") 'paredit-backward-slurp-sexp)
-(define-key paredit-mode-map (kbd "M-}") 'paredit-backward-barf-sexp)
-
 ;; Ace Jump Mode
 (require 'ace-jump-mode)
 (define-key global-map (kbd "C-x j") 'ace-jump-mode)
+(define-key global-map (kbd "C-x J") (lambda () (interactive) (ace-jump-mode 4)))
 
 ;; Hide welcome message
 (setq inhibit-startup-message t)
@@ -122,7 +278,9 @@
 ;; Pylint and Flymake
 (require 'flymake)
 
-(exec-path-from-shell-initialize)
+;; Add /usr/local/bin (homebrew) to exec-path
+(add-to-list 'exec-path "/usr/local/bin")
+
 (when (load "flymake" t)
   (defun flymake-pylint-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
@@ -135,10 +293,15 @@
   (add-to-list 'flymake-allowed-file-name-masks
                '("\\.py\\'" flymake-pylint-init)))
 
-(add-hook 'python-mode-hook (lambda () (flymake-mode t)))
-
 ;; Flymake-Cursor
 (require 'flymake-cursor)
+(add-hook 'python-mode-hook
+          (lambda ()
+            (flymake-mode t)
+            (flymake-cursor-mode)
+            (define-key python-mode-map (kbd "M-RET") 'flymake-cursor-show-errors-at-point-now)
+            (define-key python-mode-map (kbd "M-E") 'flymake-display-err-menu-for-current-line)
+            ))
 
 ;; Mac OS X
 (setq mac-command-modifier 'meta)
@@ -153,9 +316,27 @@
 (require 'org)
 (define-key org-mode-map (kbd "M-p") 'org-metaup)
 (define-key org-mode-map (kbd "M-n") 'org-metadown)
+(add-hook 'org-mode-hook
+          (lambda ()
+            (setq org-log-done 'time)
+            (setq org-todo-keywords
+                  '((sequence "TODO" "IN PROGRESS" "|" "DONE" "WONTFIX")))))
 
-;; Haskell-Mode
-(add-hook 'haskell-mode-hook (lambda () (haskell-indentation-mode t)))
+;; Prog-Mode hooks
+(require 'highlight-80+)
+(add-hook 'prog-mode-hook
+          (lambda ()
+            (fic-ext-mode t)
+            (highlight-80+-mode)
+            ;; Oscar 120 line length limit
+            (setq highlight-80+-columns 120)
+            (set (make-local-variable 'font-lock-comment-face)
+                 'fancy-comment-face)
+            ))
+
+
+;; Set default fill-column to 80 to make fill-paragraph nice
+(setq-default fill-column 80)
 
 ;; Turn off fringe mode because it doesn't work nicely in OS X
 ;; and also hide scroll bar
@@ -169,44 +350,65 @@
 
 ;; Settings for just Mac OS X Emacs with window
 (when window-system
-  (require 'color-theme-solarized)
-  (color-theme-solarized-dark)
-  (setq default-frame-alist '((width . 190)
-                              (height . 60)))
-  (custom-set-faces
-   '(default ((t (:inherit nil :stipple nil :background "#042028" :foreground "#708183" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight bold :height 135 :width extra-expanded :foundry "apple" :family "Inconsolata"))))
-   '(flymake-errline ((t (:background "#9E0000"))))
-   '(org-todo ((t (:background "#BD1111")))))
-  (custom-set-faces
-   '(flymake-errline ((t (:background "#9E0000"))))
-   '(org-todo ((t (:background "#BD1111"))))))
+  (require 'solarized-theme)
+  (load-theme 'solarized-dark 't)
+  ;; hl-line-mode for line highlighting
+  (global-hl-line-mode 1))
+
+;; org mode hook
+(add-hook
+ 'org-mode-hook
+ (lambda ()
+   ;; Turn off hl-line-mode in org-mode
+   (global-hl-line-mode 0)))
 
 ;; Ghetto way to get the path properly set
 (setenv "PATH" (shell-command-to-string "source ~/.zshrc && echo $PATH"))
 
-;; hl-line-mode for line highlighting
-(global-hl-line-mode 1)
-
 ;; Add puppet mode to .pp files
 (add-to-list 'auto-mode-alist '("\\.pp\\'" . puppet-mode))
 
+;; Add pythong mode to BUCK files
+(add-to-list 'auto-mode-alist '("BUCK\\'" . python-mode))
+
+;; Add python mode to .tw files
+(add-to-list 'auto-mode-alist '("\\.tw\\'" . python-mode))
+;; Add python mode to .aurora files
+(add-to-list 'auto-mode-alist '("\\.aurora\\'" . python-mode))
+;; Add python mode to pants BUILD files
+(add-to-list 'auto-mode-alist '("BUILD\\'" . python-mode))
+
 ;; Add markdown mode to .md files
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;; Add tuareg mode to .ml .mli files
+(add-to-list 'auto-mode-alist '("\\.ml\\'" . tuareg-mode))
+(add-to-list 'auto-mode-alist '("\\.mli\\'" . tuareg-mode))
+
+;; 
+(require 'calendar)
+(defun insdate-insert-current-date (&optional omit-day-of-week-p)
+  "Insert today's date using the current locale.
+  With a prefix argument, the date is inserted without the day of
+  the week."
+  (interactive "P*")
+  (insert (calendar-date-string (calendar-current-date) nil
+                                omit-day-of-week-p)))
+(global-set-key (kbd  "C-x M-d") 'insdate-insert-current-date)
+
+;; Set tuareg mode (ml/ocaml) default indent
+(setq tuareg-default-indent 2)
 
 ;; Setup haskell-mode
 (defun haskell-mode-hooks ()
   (turn-on-haskell-doc-mode)
   (turn-on-haskell-indentation)
-  ;;(turn-on-haskell-indent)
-  ;;(turn-on-haskell-simple-indent)
-  )
+  (haskell-indentation-mode t)
+  (highlight-80+-mode))
 
 (add-hook 'haskell-mode-hook 'haskell-mode-hooks)
 
-;; Add unmanaged load path
-(add-to-list 'load-path "~/.emacs.d/unmanaged")
-
-(require 'racket)
+;; (require 'racket)
 
 (require 'multiple-cursors)
 
@@ -214,3 +416,88 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+
+;; c++-mode hooks
+(defun c++-mode-hooks ()
+  (font-lock-add-keywords
+   'c++-mode
+   '(("\\<\\(alignof\\|alignas\\|constexpr\\|decltype\\|\
+noexcept\\|nullptr\\|static_assert\\|thread_local\\|\
+override\\|final\\)\\>" . font-lock-keyword-face)
+     ("\\<\\(char[0-9]+_t\\)\\>" . font-lock-type-face)))
+  (set (make-local-variable 'compile-command)
+       "fbmake --fast dbg -j60")
+  (highlight-80+-mode))
+
+(add-hook 'c++-mode-hook 'c++-mode-hooks)
+
+;; Don't open new files in a new frame/window
+(setq ns-pop-up-frames 'nil)
+
+;; FB C stuff from master.emacs
+;;=========================================================
+;;C, C++, Objective-C Indentation Style
+;;========================================================
+(require 'fb-objc)
+
+(defconst fb-c-style
+  '((c-basic-offset . 2)
+    (c-cleanup-list . (scope-operator
+                       brace-else-brace brace-elseif-brace brace-catch-brace
+                       defun-close-semi list-close-comma))
+    (c-hanging-braces-alist . (
+                               (arglist-cont-nonempty after)
+                               (brace-entry-open after)
+                               (class-open after)
+                               (defun-open after)
+                               (extern-lang-open after)
+                               (inexpr-class-open after)
+                               (inline-open after)
+                               (statement-cont after)
+                               (substatement-open after)
+                               (brace-list-open after)
+                               (namespace-close)
+                               (namespace-open)
+                               (block-close . c-snug-do-while)
+                               ))
+    (c-offsets-alist . (
+                        ;; indent for public: protected: private:
+                        ;; c-basic-offset + access-label == 1 char indent
+                        (access-label . -1)
+                        (arglist-intro . +)
+                        (case-label . +)
+                        (arglist-close . c-lineup-close-paren)
+                        (innamespace . 0)
+                        (member-init-intro . ++)
+                        (inher-intro . ++)
+                        (objc-method-args-cont . fb-c-lineup-ObjC-method-args)
+                        (objc-method-call-cont
+                         (fb-c-lineup-ObjC-method-call-colons
+                          fb-c-lineup-ObjC-method-call +))
+                        )))
+  "Facebook's C, C++, and Objective-C programming style"
+)
+(c-add-style "fb-c-style" fb-c-style)
+
+;; (add-hook 'c-mode-common-hook
+;;           (lambda ()
+;;             (c-set-style "fb-c-style")
+;;             (highlight-80+-mode t)
+;;             ))
+(put 'narrow-to-region 'disabled nil)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+(require 'clang-format)
+;; Add /usr/local/bin to exec-path to find clang-format and other brew
+;; installed binaries
+(setq exec-path (append exec-path '("/usr/local/bin")))
+(global-set-key (kbd "<C-M-tab>") 'clang-format-region)
+(global-set-key (kbd "C-x M-t") 'clang-format-buffer)
+
